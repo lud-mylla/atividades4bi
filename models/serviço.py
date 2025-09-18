a42ed1d6e1eda75b6d22e805b4a29b3683a2d5cd
lud-mylla <Ludmylla100608@gmail.com>
2025-09-18 13:24:31 +0000

    primeira_lista9

---
+++ models/serviço.py
@@ -0,0 +1,8 @@
+import  
+
+class Cliente:
+    def __init__(self, id, nome, email, fone):
+        self.set_id(id)
+        self.set_nome(nome)
+        self.set_email(email)
+        self.set_fone(fone)
